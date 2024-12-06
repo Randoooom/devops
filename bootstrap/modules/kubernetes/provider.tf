d73ad96f8d14feb6b1e7b34e743e735631dd530b
Randoooom <53190645+Randoooom@users.noreply.github.com>
2024-12-06 02:35:37 +0100

    fix

--- bootstrap/modules/kubernetes/provider.tf
+++ bootstrap/modules/kubernetes/provider.tf
@@ -41,4 +41,5 @@ provider "kubectl" {
   client_certificate     = base64decode(yamldecode(var.kubeconfig).users.0.user.client-certificate-data)
   client_key             = base64decode(yamldecode(var.kubeconfig).users.0.user.client-key-data)
   cluster_ca_certificate = base64decode(yamldecode(var.kubeconfig).clusters.0.cluster.certificate-authority-data)
+  load_config_file       = false
 }
