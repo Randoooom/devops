fc22f121edf10340287dd850a73b0038243e865b
Randoooom <53190645+Randoooom@users.noreply.github.com>
2024-12-29 18:01:30 +0100

    chore: bump

--- bootstrap/modules/kubernetes/argocd.tf
+++ bootstrap/modules/kubernetes/argocd.tf
@@ -175,7 +175,7 @@ resource "kubectl_manifest" "argocd_app_of_apps" {
       source = {
         repoURL        = "https://github.com/randoooom/devops"
         path           = "gitops"
-        targetRevision = "feat/event"
+        targetRevision = "main"
 
         helm = {
           values = <<EOF
