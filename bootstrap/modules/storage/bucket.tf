1f4462705f6a68734d9eb70d3e0fbae8e39c3a36
Randoooom <53190645+Randoooom@users.noreply.github.com>
2025-05-14 00:26:06 +0200

    fix: 413

--- bootstrap/modules/storage/bucket.tf
+++ bootstrap/modules/storage/bucket.tf
@@ -1,9 +1,3 @@
 data "oci_objectstorage_namespace" "this" {
   compartment_id = var.compartment_ocid
 }
-
-resource "oci_objectstorage_bucket" "nextcloud" {
-  compartment_id = var.compartment_ocid
-  name           = "${var.cluster_name}-nextcloud"
-  namespace      = data.oci_objectstorage_namespace.this.namespace
-}
--- bootstrap/modules/storage/iam.tf
+++ bootstrap/modules/storage/iam.tf
@@ -20,7 +20,7 @@ resource "oci_identity_policy" "nextcloud" {
   compartment_id = var.tenancy_ocid
   description    = "NextCloud Bucket access"
   name           = "nextcloud"
-  statements     = ["ALLOW group ${oci_identity_group.nextcloud.name} TO manage objects IN TENANCY where all {target.bucket.name = '${oci_objectstorage_bucket.nextcloud.name}'} "]
+  statements     = ["ALLOW group ${oci_identity_group.nextcloud.name} TO manage buckets IN TENANCY where all {target.bucket.name = '${var.cluster_name}-nextcloud'}", "ALLOW group ${oci_identity_group.nextcloud.name} TO manage objects IN TENANCY where all {target.bucket.name = '${var.cluster_name}-nextcloud'}"]
 }
 
 resource "oci_identity_user_group_membership" "nextcloud" {
