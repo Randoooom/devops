e89abd114d0d9115bc5e36620966f51463b47e6b
Randoooom <53190645+Randoooom@users.noreply.github.com>
2025-05-29 18:15:35 +0200

    fix: outputs

--- bootstrap/oci/outputs.tf
+++ bootstrap/oci/outputs.tf
@@ -26,39 +26,6 @@ output "public_subnet" {
   value = module.network.public_subnet
 }
 
-output "vault" {
-  sensitive = true
-  value     = module.vault.vault
-}
-
 output "public_subnet_cidr" {
   value = module.network.public_subnet_cidr
-}
-
-output "zitadel_project" {
-  value = module.zitadel.zitadel_project
-}
-
-output "feedback_fusion_client_id" {
-  sensitive = true
-  value     = module.zitadel.feedback_fusion_client_id
-}
-
-output "feedback_fusion_client_secret" {
-  sensitive = true
-  value     = module.zitadel.feedback_fusion_client_secret
-}
-
-output "zitadel_feedback_fusion_id" {
-  value = module.zitadel.zitadel_feedback_fusion_id
-}
-
-output "grafana_client_id" {
-  sensitive = true
-  value     = module.zitadel.grafana_client_id
-}
-
-output "grafana_client_secret" {
-  sensitive = true
-  value     = module.zitadel.grafana_client_secret
 }
