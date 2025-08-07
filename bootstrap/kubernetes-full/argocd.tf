fe6cc9960d874051226b0643633c26193dd6dfff
Randoooom <53190645+Randoooom@users.noreply.github.com>
2025-08-07 14:06:11 +0200

    fix: branch

--- bootstrap/kubernetes-full/argocd.tf
+++ bootstrap/kubernetes-full/argocd.tf
@@ -192,7 +192,7 @@ resource "kubectl_manifest" "argocd_app_of_apps" {
       source = {
         repoURL        = "https://github.com/randoooom/devops"
         path           = "gitops"
-        targetRevision = "feat/imrprove-vector-parsing"
+        targetRevision = "main"
 
         helm = {
           values = <<EOF
