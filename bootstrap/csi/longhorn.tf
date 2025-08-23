c52511349f15441bac4fa0a0cf66acb84780cd87
Randoooom <53190645+Randoooom@users.noreply.github.com>
2025-08-23 15:18:39 +0200

    fix: remove longhorn ingress

--- bootstrap/csi/longhorn.tf
+++ bootstrap/csi/longhorn.tf
@@ -22,18 +22,6 @@ resource "helm_release" "longhorn" {
     longhornUI = {
       replicas = 1
     }
-
-    ingress = {
-      enabled          = true
-      ingressClassName = "internal"
-      host             = "longhorn.internal.${var.cluster_domain}"
-      annotations = {
-        "nginx.ingress.kubernetes.io/auth-response-headers"   = "Authorization"
-        "nginx.ingress.kubernetes.io/auth-signin"             = "https://secure.${var.cluster_domain}/oauth2/start?rd=$scheme://$host$escaped_request_uri"
-        "nginx.ingress.kubernetes.io/auth-url"                = "https://secure.${var.cluster_domain}/oauth2/auth"
-        "external-dns.alpha.kubernetes.io/cloudflare-proxied" = "false"
-      }
-    }
   })]
 }
 
